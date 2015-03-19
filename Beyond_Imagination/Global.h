0c2f41905ea2e8b5a01bc2243c17bac1eb1f9fcf
Sersos <janniklorenz@nord-com.net>
2015-03-19 14:03:44 +0100

    [Add] Add Basic Project Structure

---
+++ Beyond_Imagination/Global.h
@@ -0,0 +1,12 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+//unsigned
+typedef unsigned short uint8;
+typedef unsigned int uint16;
+
+//signed
+typedef signed short uint8;
+typedef signed int uint16;
+
+#endif
