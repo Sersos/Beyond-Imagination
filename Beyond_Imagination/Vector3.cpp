d4dc0bf07c5ceff1e9f9026c76991fd9185f0f79
Sersos <janniklorenz@nord-com.net>
2015-03-19 20:02:32 +0100

    [Update] Update Vector3-Class

--- Beyond_Imagination/Vector3.cpp
+++ Beyond_Imagination/Vector3.cpp
@@ -1,10 +1,24 @@
 #include "Vector3.h"
+#include <math.h>
 
 Vector3::Vector3(float x, float y, float z)
 {
 	this->x = x;
 	this->y = y;
 	this->z = z;
+}
+
+Vector3::Vector3(float x, float y, float z = 0)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+float Vector3::length()
+{
+	double temp = x * x + y * y + z * z;
+	return sqrt(temp);
 }
 
 
--- Beyond_Imagination/Vector3.h
+++ Beyond_Imagination/Vector3.h
@@ -1,11 +1,20 @@
-#pragma once
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
 class Vector3
 {
-public:
+public:	
 	Vector3(float x, float y, float z);
+
+	//constructor for point with z = 0
+	Vector3(float x, float y, float z = 0);
+
+	//Get Length of 3 dimensional vector
+	float length();
 
 	float x;
 	float y;
 	float z;
 };
 
+#endif
