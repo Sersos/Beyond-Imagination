110e263044841556c794123167528cf58f941e32
RevolutionV <therevolution@gmx.de>
2015-03-19 19:41:38 +0100

    [CHANGE] Changed Position/Rotation-Manager in GameObject

--- Beyond_Imagination/GameObject.h
+++ Beyond_Imagination/GameObject.h
@@ -1,9 +1,9 @@
 
 #include "Vector3.h"
 
-class PositionManager;
-class RotationManager;
-class ScaleManager;
+class GlobalPosition;
+class GlobalRotation;
+class GlobalScale;
 
 class GameObject
 {
@@ -11,9 +11,9 @@ public:
 	GameObject(Vector3& position, Vector3& rotation, Vector3& scale);
 
 private:
-	RotationManager* rotation;
-	PositionManager* position;
-	ScaleManager* scale;
+	GlobalPosition* position;
+	GlobalRotation* rotation;
+	GlobalScale* scale;
 	
 };
 
