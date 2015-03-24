d3df5fe17550ce79a4f33a7c3c9eba4fe07d959d
Sersos <janniklorenz@nord-com.net>
2015-03-24 14:46:08 +0100

    [add] Add more 3D Stuff

--- Beyond_Imagination/Application/DirectxManager.cpp
+++ Beyond_Imagination/Application/DirectxManager.cpp
@@ -7,11 +7,16 @@ DirectxManager::DirectxManager()
 	deviceContext = 0;
 	swapChain = 0;
 	renderTargetView = 0;
+	depthStencilBuffer = 0;
+	depthStencilState = 0;
+	depthStencilView = 0;
 }
 
 bool DirectxManager::initialize(HWND window)
 {
 	HRESULT hr;
+	D3D11_TEXTURE2D_DESC depthBufferDesc;
+	D3D11_DEPTH_STENCIL_DESC depthStencilDesc;
 
 	//create swapChain desc
 	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
@@ -51,6 +56,52 @@ bool DirectxManager::initialize(HWND window)
 
 	pointerBackBuffer->Release();
 	pointerBackBuffer = 0;
+
+	//depth buffer desc
+	ZeroMemory(&depthBufferDesc, sizeof(D3D11_TEXTURE2D_DESC));
+	depthBufferDesc.Width = DEFAULT_WINDOW_WIDTH;
+	depthBufferDesc.Height = DEFAULT_WINDOW_HEIGHT;
+	depthBufferDesc.MipLevels = 1;
+	depthBufferDesc.ArraySize = 1;
+	depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthBufferDesc.SampleDesc.Count = 1;
+	depthBufferDesc.SampleDesc.Quality = 0;
+	depthBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthBufferDesc.CPUAccessFlags = 0; 
+	depthBufferDesc.MiscFlags = 0;
+
+	hr = device->CreateTexture2D(&depthBufferDesc, NULL, &depthStencilBuffer);
+	if (FAILED(hr))
+		return false;
+
+	//depth stencil desc
+	ZeroMemory(&depthStencilDesc, sizeof(D3D11_DEPTH_STENCIL_DESC));
+	depthStencilDesc.DepthEnable = true;
+	depthStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	depthStencilDesc.DepthFunc = D3D11_COMPARISON_LESS;
+
+	depthStencilDesc.StencilEnable = true; 
+	depthStencilDesc.StencilReadMask = 0xFF;
+	depthStencilDesc.StencilWriteMask = 0xFF;
+
+	//for front pixel
+	depthStencilDesc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	depthStencilDesc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	depthStencilDesc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	depthStencilDesc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	//for back pixel
+	depthStencilDesc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	depthStencilDesc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	depthStencilDesc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	depthStencilDesc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	//create the depth stencil state
+	hr = device->CreateDepthStencilState(&depthStencilDesc, &depthStencilState);
+
+	//initalize other 3D stuff
+
 
 	deviceContext->OMSetRenderTargets(1, &renderTargetView, NULL);
 
--- Beyond_Imagination/Application/DirectxManager.h
+++ Beyond_Imagination/Application/DirectxManager.h
@@ -34,6 +34,9 @@ private:
 	ID3D11DeviceContext* deviceContext;
 	IDXGISwapChain* swapChain;
 	ID3D11RenderTargetView* renderTargetView;
+	ID3D11Texture2D* depthStencilBuffer;
+	ID3D11DepthStencilState* depthStencilState;
+	ID3D11DepthStencilView* depthStencilView;
 	DXGI_SWAP_CHAIN_DESC swapChainDesc;
 	
 
