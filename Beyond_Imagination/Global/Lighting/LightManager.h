0dc990657bd8150cb196ce6f913282c805ba9db2
Sersos <janniklorenz@nord-com.net>
2015-04-01 21:25:50 +0200

    [Add] Add basic ApplicationManager structure

---
+++ Beyond_Imagination/Global/Lighting/LightManager.h
@@ -0,0 +1,19 @@
+#include "DirectxManager.h"
+
+struct DirectionalLight
+{
+	//position
+	D3DXVECTOR3 position;
+};
+
+struct SpotLight
+{
+	//position
+	D3DXVECTOR3 position;
+};
+
+struct PointLight
+{
+	//position
+	D3DXVECTOR3 position;
+};
