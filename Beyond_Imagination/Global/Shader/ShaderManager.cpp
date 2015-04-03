eaa9956636dcab369009e618f58cf8437c27e4b8
Sersos <janniklorenz@nord-com.net>
2015-04-03 12:29:22 +0200

    changes

--- Beyond_Imagination/Global/Shader/ShaderManager.cpp
+++ Beyond_Imagination/Global/Shader/ShaderManager.cpp
@@ -14,14 +14,14 @@ void ShaderManager::initialize(ID3D11Device* device, ID3D11DeviceContext* device
 	HRESULT result;
 	ID3D10Blob* compiledShader;	
 	
-	/*
+	
 	result = D3DX11CompileFromFile(L"Shader.fx", 0, 0, 0, "fx_5_0", 0, 0, 0, &compiledShader, NULL, 0);
 	if (FAILED(result))
 		MessageBox(0, L"Cant compile Shader", 0, MB_OK);
 	
-	*/
+	
 
-	std::ifstream fin("Shader.fxo", std::ios::binary);
+	//std::ifstream fin("Shader.fxo", std::ios::binary);
 	
 
 	result = D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(), 0, device, &effect);
