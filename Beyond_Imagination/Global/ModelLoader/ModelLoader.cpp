d490c30f3435371bde8214ffcb95a304fea14318
Sersos <janniklorenz@nord-com.net>
2015-04-07 10:27:48 +0200

    Update ModelLoader.cpp

--- Beyond_Imagination/Global/ModelLoader/ModelLoader.cpp
+++ Beyond_Imagination/Global/ModelLoader/ModelLoader.cpp
@@ -47,6 +47,7 @@ void ModelLoader::loadObject(const char* filename)
 				
 				ModelData modelData;
 				modelData.Position = D3DXVECTOR3((float)x, (float)y, (float)z * -1.0f);
+				modelData.Normal = D3DXVECTOR(0.0f, 0.0f, 0.0f);
 				
 				m_vertices.push_back(modelData);
 
