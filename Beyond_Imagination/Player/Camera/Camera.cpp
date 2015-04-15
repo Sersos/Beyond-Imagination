0564f8803693e06dc0c54c69af31b348b19e7337
RevolutionV <therevolution@gmx.de>
2015-04-15 14:06:30 +0200

    Revert "Revert "[FIXED] Fixed Camera Movement Yaw and Pitch""
    This reverts commit f1b1fde91b0d7baff5bea418b00d0e8059d8ff23.

--- Beyond_Imagination/Player/Camera/Camera.cpp
+++ Beyond_Imagination/Player/Camera/Camera.cpp
@@ -9,18 +9,25 @@ Camera::Camera(HWND* window)
 	m_inputManager = new InputManager(window);	
 }
 
-void Camera::initialize(D3DXVECTOR3& position)
+void Camera::initialize(D3DXVECTOR3 position)
 {
-	//initialize up-Vector
-	m_up.x = 0.0f;
-	m_up.y = 1.0f;
-	m_up.z = 0.0f;
-
+	m_up = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
 	m_position = position;
-	m_eulerAngle.x = 0.0f;
-	m_eulerAngle.y = 0.0f;
-	m_eulerAngle.z = 0.0f;
+	m_rotation = D3DXVECTOR3(0.0f, 0.0f, 90.0f);
 	m_target = D3DXVECTOR3(0,0,1);
+	m_forward = m_target - m_position;
+	
+	//        Bogenmaß                PI/180
+	m_YawPitchRoll = m_rotation * 0.0174532925f;
+	D3DXMatrixRotationYawPitchRoll(&m_rotationMatrix, m_YawPitchRoll.x, m_YawPitchRoll.y, m_YawPitchRoll.z);
+
+	//set target und up vector with rotationmatrix
+	D3DXVec3TransformCoord(&m_target, &m_target, &m_rotationMatrix);
+	D3DXVec3TransformCoord(&m_up, &m_up, &m_rotationMatrix);
+	
+	m_target = m_position + m_target;
+
+	D3DXMatrixLookAtLH(&m_viewMatrix,&m_position,&m_target,&m_up);
 
 	selectPerspective(0);
 }
@@ -43,120 +50,86 @@ void Camera::selectPerspective(int t)
 }
 void Camera::update()
 {
-	D3DXVECTOR2 oldmousePosition = m_inputManager->getMousePosition();
-	//get length of position vector
-	float length = D3DXVec3Length(&m_position);
+
+
+
+
 
 	m_forward = m_target - m_position;
 	D3DXVec3Normalize(&m_forward, &m_forward);
+	controll();
+	
+	//get current view matrix after updates	
+	D3DXMatrixLookAtLH(&m_viewMatrix, &m_position, &m_target, &m_up);
+}
+
+void Camera::controll()
+{
+	//target in this case everytime in front of the cam!
+	m_target = D3DXVECTOR3(0, 0, 1);
 
 	if (m_inputManager->isKeyPressed(KEYBOARD_W))
 	{
 		m_position += m_forward * CAMERA_MOVEMENT_SPEED;
-		m_target += m_forward * CAMERA_MOVEMENT_SPEED;
 	}
 	if (m_inputManager->isKeyPressed(KEYBOARD_S))
 	{
-		m_position += -m_forward * CAMERA_MOVEMENT_SPEED;
-		m_target += -m_forward * CAMERA_MOVEMENT_SPEED;
+		m_position -= m_forward * CAMERA_MOVEMENT_SPEED;
 	}
 
 	if (m_inputManager->isKeyPressed(KEYBOARD_A))
 	{
 		D3DXVECTOR3 crossProduct = *D3DXVec3Cross(&m_forward, &m_forward, &m_up);
 		m_position += *D3DXVec3Normalize(&m_right, &crossProduct) * CAMERA_MOVEMENT_SPEED;
-		m_target += *D3DXVec3Normalize(&m_right, &crossProduct) * CAMERA_MOVEMENT_SPEED;
 	}
 
 	if (m_inputManager->isKeyPressed(KEYBOARD_D))
 	{
 		D3DXVECTOR3 crossProduct = *D3DXVec3Cross(&m_forward, &m_forward, &m_up);
-		m_position += -(*D3DXVec3Normalize(&m_right, &crossProduct) * CAMERA_MOVEMENT_SPEED);
-		m_target += -(*D3DXVec3Normalize(&m_right, &crossProduct) * CAMERA_MOVEMENT_SPEED);
+		m_position -= *D3DXVec3Normalize(&m_right, &crossProduct) * CAMERA_MOVEMENT_SPEED;
 	}
-	//rotation
 
-	//around Y 
+	//Yaw
 	if (m_inputManager->isKeyPressed(KEYBOARD_J))
 	{
-		if (m_eulerAngle.y > (2 * D3DX_PI))
-			m_eulerAngle.y = 0;
-		if (m_eulerAngle.y < (-2 * D3DX_PI))
-			m_eulerAngle.y = 0;
-
-		m_eulerAngle.y += 0.001f;
-
-		m_target.z = m_position.z + sinf(m_eulerAngle.y);
-		m_target.x = m_position.x + cosf(m_eulerAngle.y);
+		m_rotation.y += 0.1f;
+		
 	}
-	else if (m_inputManager->isKeyPressed(KEYBOARD_L))
+	if (m_inputManager->isKeyPressed(KEYBOARD_L))
 	{
-		if (m_eulerAngle.y > (2 * D3DX_PI))
-			m_eulerAngle.y = 0;
-		if (m_eulerAngle.y < (-2 * D3DX_PI))
-			m_eulerAngle.y = 0;
-
-		m_eulerAngle.y -= 0.001f;
-
-		m_target.z = m_position.z + sinf(m_eulerAngle.y);
-		m_target.x = m_position.x + cosf(m_eulerAngle.y);
+		m_rotation.y -= 0.1f;
 	}
-
-	//around  X
-	else if (m_inputManager->isKeyPressed(KEYBOARD_I))
+	//Pitch
+	if (m_inputManager->isKeyPressed(KEYBOARD_I))
 	{
-		if (m_eulerAngle.x > (2 * D3DX_PI))
-			m_eulerAngle.x = 0;
-		if (m_eulerAngle.x < (-2 * D3DX_PI))
-			m_eulerAngle.x = 0;
-
-		m_eulerAngle.x += 0.001f;
-
-		m_target.y = m_position.y + sinf(m_eulerAngle.x);
-		m_target.z = m_position.z + cosf(m_eulerAngle.x);
+		m_rotation.x -= 0.1f;
 	}
-	else if (m_inputManager->isKeyPressed(KEYBOARD_K))
+	if (m_inputManager->isKeyPressed(KEYBOARD_K))
 	{
-		if (m_eulerAngle.x >(2 * D3DX_PI))
-			m_eulerAngle.x = 0;
-		if (m_eulerAngle.x < (-2 * D3DX_PI))
-			m_eulerAngle.x = 0;
-
-		m_eulerAngle.x -= 0.001f;
-
-		m_target.y = m_position.y + sinf(m_eulerAngle.x);
-		m_target.z = m_position.z + cosf(m_eulerAngle.x);
+		m_rotation.x += 0.1f;
 	}
-	//around  Z
-	else if (m_inputManager->isKeyPressed(KEYBOARD_U))
+	//Roll
+	//_________________________________________DONT RUN____________________________________
+	if (m_inputManager->isKeyPressed(KEYBOARD_U))
 	{
-		if (m_eulerAngle.z > (2 * D3DX_PI))
-			m_eulerAngle.z = 0;
-		if (m_eulerAngle.z < (-2 * D3DX_PI))
-			m_eulerAngle.z = 0;
-
-		m_eulerAngle.z += 0.001f;
-
-		m_target.y = m_position.y + sinf(m_eulerAngle.z);
-		m_target.x = m_position.x + cosf(m_eulerAngle.z);
+		m_rotation.z -= 0.1f;
 	}
-	else if (m_inputManager->isKeyPressed(KEYBOARD_O))
+	if (m_inputManager->isKeyPressed(KEYBOARD_O))
 	{
-		if (m_eulerAngle.z >(2 * D3DX_PI))
-			m_eulerAngle.z = 0;
-		if (m_eulerAngle.z < (-2 * D3DX_PI))
-			m_eulerAngle.z = 0;
-
-		m_eulerAngle.z -= 0.001f;
-
-		m_target.y = m_position.y + sinf(m_eulerAngle.z);
-		m_target.x = m_position.x + cosf(m_eulerAngle.z);
+		m_rotation.z += 0.1f;
 	}
+	//______________________________________________________________________________________
 
-	//get current view matrix after updates	
-	D3DXMatrixLookAtLH(&m_viewMatrix, &m_position, &m_target, &m_up);
+
+	//bogenmaß
+	m_YawPitchRoll = m_rotation * 0.0174532925f;
+	D3DXMatrixRotationYawPitchRoll(&m_rotationMatrix, m_YawPitchRoll.x, m_YawPitchRoll.y, m_YawPitchRoll.z);
+
+	//calaculate the target position with rotationmatrix!
+	D3DXVec3TransformCoord(&m_target, &m_target, &m_rotationMatrix);
+	
+	m_target += m_position;
 }
-
 
 D3DXMATRIX Camera::getViewMatrix()
 {
